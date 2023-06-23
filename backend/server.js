f4a49a4f94f7874eceece368a570c05636867712
ismail-elbarsi <elbarsi.ismail.il.esisa.2020.2021@gmail.com>
2023-06-23 12:31:18 +0100

    new

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use("/api/employees", employeRouter);
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
 
 app.use((err, req, res, next) => {
